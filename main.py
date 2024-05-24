57e001bfca268524672e39a1cebbae46fadba427
eusen <eugene.snesarev@gmail.com>
2024-05-24 12:19:21 +0300

    Add nameserver option

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ def discover_domain(domain_name, nameserver_addr=""):
     dns_name_servers = []
     if nameserver_addr != "":
         dns_name_servers.append(nameserver_addr)
-    domain = ADDomain(domain_dns_name, dns_nameservers=dns_name_servers)
+        domain = ADDomain(domain_dns_name, dns_nameservers=dns_name_servers)
+    else:
+        domain = ADDomain(domain_dns_name)
     ldap_servers = domain.get_ldap_uris()
     kerberos_servers = domain.get_kerberos_uris()
     print("Count of the ldap servers for " + domain_dns_name + ": " + str(len(ldap_servers)))
@@ -28,7 +30,11 @@ if __name__ == '__main__':
     parser.add_argument("-n", "--nameserver", help="Alternative dns server")
 
     args = parser.parse_args()
-    if args.domain or args.nameserver:
+    if args.domain is not None and args.nameserver is not None:
         discover_domain(args.domain, args.nameserver)
+    elif args.domain is not None:
+        discover_domain(args.domain)
+
+
 
 
