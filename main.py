87f1d39f4b9a0d253c935b6eb65d12549e26cceb
eusen <eugene.snesarev@gmail.com>
2024-05-24 11:16:20 +0300

    Argument parser

--- main.py
+++ main.py
@@ -3,19 +3,26 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from ms_active_directory import ADDomain
+import argparse
 
 def discover_domain(domainName):
+
    domain_dns_name = domainName
    domain = ADDomain(domain_dns_name)
    ldap_servers = domain.get_ldap_uris()
    kerberos_servers = domain.get_kerberos_uris()
    print("Count of the ldap servers for " + domain_dns_name +": " +  str(len(ldap_servers)))
+   print("Count of the kerberos servers for " + domain_dns_name + ": " + str(len(ldap_servers)))
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # Domain lookup examples
-    discover_domain("klion.vd")
-    discover_domain("kliongroup")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--domain", help="AD domain name")
+
+    args = parser.parse_args()
+    if args.domain:
+        discover_domain(args.domain)
 
 
